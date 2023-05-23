470f33d14b6f1b8e89ad3150c23dc3eeadc75343
NurFer13 <nurfer1307@gmail.com>
2023-05-23 17:32:50 +0000

    i need to change the positions

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "k"];
+let suits = ["♦", "♥", "♠", "♣"];
+
+const button = document.querySelector("button");
+const display = document.querySelector("#display");
+
+const getRandomCard = () => {
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const number = numbers[Math.floor(Math.random() * numbers.length)];
+
+  let generateCard = (display.innerHTML = `<div class="topsuit"><span id="topsuit">${suit}</span></div>
+  <div class="num"><span id="num">${number}</span></div>
+  <div class="bottomsuit"><span id="bottomsuit">${suit}</span></div>`);
+
+  if (suit === "♥" || suit === "♦") {
+    generateCard = "<span class='red'>" + generateCard + "</span>";
+  } else {
+    generateCard = "<span class='black'>" + generateCard + "</span>";
+  }
+  return generateCard;
 };
+
+button.addEventListener("click", function() {
+  const card = getRandomCard();
+  display.innerHTML = card;
+});
